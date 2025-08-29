54849a96f952b542cee13668b4cf7c9a8fb6d83c
Chris Gianelloni <wolf31o2@blinklabs.io>
2025-08-28 21:08:10 -0400

    fix: site URL in astro.config.mjs
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://blinklabs.io',
+	site: 'https://docs.blinklabs.io',
 	redirects: {
 		'/': '/home',
 	},
